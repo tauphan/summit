d17c6c4237a1e84acdf8b2acbc648c2f853e15d8
Baptiste Bauvin <baptiste.bauvin@lis-lab.fr>
2019-12-23 04:14:06 -0500

    Cuisine ready for cluster

--- multiview_platform/mono_multi_view_classifiers/monoview_classifiers/bagging.py
+++ multiview_platform/mono_multi_view_classifiers/monoview_classifiers/bagging.py
@@ -15,18 +15,15 @@ __author__ = "Baptiste Bauvin"
 __status__ = "Prototype"  # Production, Development, Prototype
 
 
-classifier_class_name = "BaggingPregen"
+classifier_class_name = "Bagging"
 
-class BaggingPregen(BaggingClassifier, BaseMonoviewClassifier,
-                     PregenClassifier):
+class Bagging(BaggingClassifier, BaseMonoviewClassifier,):
     """
 
     """
     def __init__(self, random_state=None, n_estimators=50, bootstrap_features=True, max_samples=1.0, max_features=1.0,
-                 base_estimator=None, n_stumps=1, estimators_generator="Stumps",
-                 max_depth_pregen=1, self_complemeted=True,
-                 **kwargs):
-        super(BaggingPregen, self).__init__(
+                 base_estimator=None, **kwargs):
+        super(Bagging, self).__init__(
             random_state=random_state,
             n_estimators=n_estimators,
             max_samples=max_samples,
@@ -34,35 +31,26 @@ class BaggingPregen(BaggingClassifier, BaseMonoviewClassifier,
             base_estimator=base_estimator,
             max_features=max_features
         )
-        self.param_names = ["n_estimators", "base_estimator", "n_stumps",
-                            "estimators_generator", "max_depth_pregen",
+        self.param_names = ["n_estimators", "base_estimator",
                             "random_state", "bootstrap_features", "max_samples", "max_features"]
         self.classed_params = ["base_estimator"]
         self.distribs = [CustomRandint(low=1, high=500),
-                         [DecisionTreeClassifier(max_depth=1)], [n_stumps],
-                         ["Stumps",], CustomRandint(low=1, high=5),
+                         [DecisionTreeClassifier(max_depth=1)],
                          [random_state], [True], [1.0], CustomUniform()]
         self.weird_strings = {"base_estimator": "class_name"}
         self.plotted_metric = metrics.zero_one_loss
         self.plotted_metric_name = "zero_one_loss"
-        self.step_predictions = None
-        self.estimators_generator = estimators_generator
-        self.max_depth_pregen = max_depth_pregen
-        self.n_stumps = n_stumps
-        self.self_complemented = self_complemeted
 
     def fit(self, X, y, sample_weight=None):
         """
         """
         begin = time.time()
-        pregen_X, pregen_y = self.pregen_voters(X, y)
-        super(BaggingPregen, self).fit(pregen_X, pregen_y,
-                                        sample_weight=sample_weight)
+        super(Bagging, self).fit(X, y, sample_weight=sample_weight)
         end = time.time()
         self.train_time = end - begin
-        self.train_shape = pregen_X.shape
+        self.train_shape = X.shape
         # self.base_predictions = np.array(
-        #     [change_label_to_zero(estim.predict(pregen_X)) for estim in
+        #     [change_label_to_zero(estim.predict(X)) for estim in
         #      self.estimators_])
 
 
@@ -82,8 +70,7 @@ class BaggingPregen(BaggingClassifier, BaseMonoviewClassifier,
 
         """
         begin = time.time()
-        pregen_X, _ = self.pregen_voters(X)
-        pred = super(BaggingPregen, self).predict(pregen_X)
+        pred = super(Bagging, self).predict(X)
         end = time.time()
         self.pred_time = end - begin
         return change_label_to_zero(pred)
