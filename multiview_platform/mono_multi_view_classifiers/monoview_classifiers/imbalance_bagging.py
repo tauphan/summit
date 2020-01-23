16d572596da3bad6a7ee0856a9b2bbbbb08cca37
Baptiste Bauvin <baptiste.bauvin@lis-lab.fr>
2020-01-23 14:32:19 +0100

    Imbalaced

---
+++ multiview_platform/mono_multi_view_classifiers/monoview_classifiers/imbalance_bagging.py
@@ -0,0 +1,27 @@
+from imblearn.ensemble import BalancedBaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+from ..monoview.monoview_utils import BaseMonoviewClassifier, CustomRandint, CustomUniform
+
+
+classifier_class_name = "ImbalanceBagging"
+
+class ImbalanceBagging(BaseMonoviewClassifier, BalancedBaggingClassifier):
+
+    def __init__(self, random_state=None, base_estimator=DecisionTreeClassifier(max_depth=1), n_estimators=10,
+                 sampling_strategy="auto", replacement=False,):
+        print(random_state)
+        super(ImbalanceBagging, self).__init__(random_state=random_state, base_estimator=base_estimator,
+                                         n_estimators=n_estimators,
+                                         sampling_strategy=sampling_strategy,
+                                         replacement=replacement)
+
+        self.param_names = ["n_estimators", "base_estimator", "sampling_strategy"]
+        self.classed_params = ["base_estimator"]
+        self.distribs = [CustomRandint(low=1, high=50),
+                         [DecisionTreeClassifier(max_depth=1)],
+                         CustomUniform()]
+        self.weird_strings = {"base_estimator": "class_name"}
+
+
+
