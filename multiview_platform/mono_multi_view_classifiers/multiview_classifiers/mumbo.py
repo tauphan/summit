81ea004a57959459ab580d54212db6116af55483
Baptiste Bauvin <baptiste.bauvin@lis-lab.fr>
2020-03-02 16:58:35 +0100

    Mumbo dict base param

--- multiview_platform/mono_multi_view_classifiers/multiview_classifiers/mumbo.py
+++ multiview_platform/mono_multi_view_classifiers/multiview_classifiers/mumbo.py
@@ -1,4 +1,5 @@
 from sklearn.tree import DecisionTreeClassifier
+from  sklearn.base import BaseEstimator
 import numpy as np
 
 from multimodal.boosting.mumbo import MumboClassifier
@@ -17,6 +18,7 @@ class Mumbo(BaseMultiviewClassifier, MumboClassifier):
                  random_state=None,
                  best_view_mode="edge"):
         BaseMultiviewClassifier.__init__(self, random_state)
+        base_estimator = self.set_base_estim_from_dict(base_estimator)
         MumboClassifier.__init__(self, base_estimator=base_estimator,
                                     n_estimators=n_estimators,
                                     random_state=random_state,
@@ -24,6 +26,23 @@ class Mumbo(BaseMultiviewClassifier, MumboClassifier):
         self.param_names = ["base_estimator", "n_estimators", "random_state", "best_view_mode"]
         self.distribs = [base_boosting_estimators,
                          CustomRandint(5,200), [random_state], ["edge", "error"]]
+
+    def set_base_estim_from_dict(self, base_estim_dict):
+        if base_estim_dict is None:
+            base_estimator = DecisionTreeClassifier()
+        elif isinstance(base_estim_dict, dict):
+            estim_name = next(iter(base_estim_dict))
+            estim_module = getattr(monoview_classifiers, estim_name)
+            estim_class = getattr(estim_module,
+                                  estim_module.classifier_class_name)
+            base_estimator = estim_class(**base_estim_dict[estim_name])
+        elif isinstance(base_estim_dict, BaseEstimator):
+            base_estimator = base_estim_dict
+        else:
+            raise ValueError("base_estimator should be either None, a dictionary"
+                             " or a BaseEstimator child object, "
+                             "here it is {}".format(type(base_estim_dict)))
+        return base_estimator
 
     def set_params(self, base_estimator=None, **params):
         """
@@ -35,11 +54,7 @@ class Mumbo(BaseMultiviewClassifier, MumboClassifier):
         if base_estimator is None:
             self.base_estimator = DecisionTreeClassifier()
         elif isinstance(base_estimator, dict):
-            estim_name = next(iter(base_estimator))
-            estim_module = getattr(monoview_classifiers, estim_name)
-            estim_class = getattr(estim_module,
-                                  estim_module.classifier_class_name)
-            self.base_estimator = estim_class(**base_estimator[estim_name])
+            self.base_estimator = self.set_base_estim_from_dict(base_estimator)
             MumboClassifier.set_params(self, **params)
         else:
             MumboClassifier.set_params(self, base_estimator=base_estimator, **params)
@@ -75,7 +90,7 @@ class Mumbo(BaseMultiviewClassifier, MumboClassifier):
         interpret_string = "Mumbo used {} iterations to converge.".format(self.best_views_.shape[0])
         interpret_string+= "\n\nViews importance : \n"
         for view_index in sorted_view_indices:
-            interpret_string+="- View {}({}), importance {}\n".format(view_index,
+            interpret_string+="- View {} ({}), importance {}\n".format(view_index,
                                                                       self.view_names[view_index],
                                                                       self.view_importances[view_index])
         interpret_string +="\n The boosting process selected views : \n" + ", ".join(map(str, self.best_views_))
